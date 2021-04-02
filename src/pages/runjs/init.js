71a966d3550737e11f3858ad2b6a42ed025dd2c5
jojowwbb <710335997@qq.com>
2021-04-02 18:51:57 +0800

    update ui

--- src/pages/runjs/init.js
+++ src/pages/runjs/init.js
@@ -1,38 +1,32 @@
 export default {
-	css: `.blend {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-} 
-.blend img {
-    position: absolute;
-    height: 100%;
-    width: 100%;
-}
-.blend h1 { 
-    font-size: 120px;
-    mix-blend-mode: overlay;
-}`,
+	css: `body{
+        background:#36404a;
+        width:100%;
+        height:100%;
+    }
+    .blend{
+        position:absolute;
+        left:50%;
+        top:50%;
+        transform:translate(-50%,-50%);
+    }
+    h3{
+        color:#fff;
+    }
+    p{
+        color:rgba(255,255,255,0.75);
+    }`,
 	html: `<div id="root"></div>`,
-    javascript:`function App() {
-        console.log('hello log');
+	javascript: `function App() {
+        console.log('Pen Editor');
+        console.error('a simple code editor');
         return (
-            <div class="blend">
-                <img src="https://cdn.pixabay.com/photo/2016/12/11/12/02/bled-1899264_960_720.jpg" />
-                <h1>Pen Editor</h1>
+            <div className="blend">
+                <h3>Pen Editor</h3>
+                <p>a simple code editor</p>
             </div>
         );
     }
     ReactDOM.render(<App />, document.getElementById("root"));
-`
-// 	javascript: `function main(){
-//     console.log('hello log');
-//     console.error(new Error('error msg'));
-//     console.error('error %o',new Error('error msg'));
-//     console.info('hello info');
-//     console.info({name:'info'});
-// }
-// main();`,
+`,
 };
